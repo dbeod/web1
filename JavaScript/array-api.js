dcb8e47e0f60768ad6163b463cafe2deba860bd8
dbeod <61372040+dbeod@users.noreply.github.com>
2020-11-09 18:18:12 +0900

    Add files via upload

---
+++ JavaScript/array-api.js
@@ -0,0 +1,20 @@
+'use strict';
+//Q1. make a string out of an array
+{
+    const fruits = ['apple','banana','orange'];
+    const result = fruits.join(', ');
+    console.log(result);
+}
+
+//Q2. make an array out of a string
+{
+    const fruits = '●, ★, ◆, ♣';
+    const result = fruits.split(',');
+    console.log(result);
+
+}
+2020.11.03
+13강까지 들어올 것 
+
+react보고올것 1강
+ 그 다음주에 1/2강 
